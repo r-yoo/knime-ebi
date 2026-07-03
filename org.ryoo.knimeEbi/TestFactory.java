2f77ae5685f071d5774dc2b5d4f091a7a07c905b
Reinhold Yoo <reinhold.yoo@rwth-aachen.de>
2026-07-03 16:35:55 +0200

    Created scaffolder that creates specific TestFactory.java

---
+++ org.ryoo.knimeEbi/TestFactory.java
@@ -0,0 +1,6 @@
+package org.ryoo.knimeEbi.node.test;
+public class TestFactory {
+public static void main(String[] args) {
+System.out.println("Hello World!");
+}
+}
--- org.ryoo.knimeEbi/src/org/ryoo/knimeEbi/scaffolder/NodeFactoryFileGenerator.java
+++ org.ryoo.knimeEbi/src/org/ryoo/knimeEbi/scaffolder/NodeFactoryFileGenerator.java
@@ -1,9 +1,51 @@
 package org.ryoo.knimeEbi.scaffolder;
 
 import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class NodeFactoryFileGenerator {
 	
+	public static void createTest() throws IOException {
+		Path outputPath = Path.of(
+			"src",
+			"org",
+			"ryoo",
+			"knimeEbi",
+			"node",
+			"test", // read function name directly from Ebi
+			"TestFactory.java" // read function name directly from Ebi
+		);
+		
+		Files.createDirectories(outputPath.getParent());
+		
+		try(BufferedWriter writer = Files.newBufferedWriter(outputPath, StandardCharsets.UTF_8)){
+			writer.write("package org.ryoo.knimeEbi.node.test;");
+			writer.newLine();
+			writer.newLine();
+			writer.write("public class TestFactory {");
+			writer.newLine();
+				writer.write("	public static void main(String[] args) {");
+				writer.newLine();
+					writer.write("		System.out.println(\"Hello World!\");");
+					writer.newLine();
+				writer.write("	}");
+			writer.newLine();
+			writer.write("}");
+		} catch (IOException e) {
+			System.out.println("Error creating NodeFactory.");
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		try {
+			createTest();
+		} catch (IOException e) {
+			System.out.println("Error calling function createTest().");
+			e.printStackTrace();
+		}
+	}
 }
