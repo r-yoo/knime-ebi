a7e1c29b5c7b2961b077020b321b2e49fcc85585
Reinhold Yoo <reinhold.yoo@rwth-aachen.de>
2026-07-29 21:07:36 +0200

    Cleanup

--- org.ryoo.knimeEbi/src/org/ryoo/knimeEbi/entryPoint/Main.java
+++ org.ryoo.knimeEbi/src/org/ryoo/knimeEbi/entryPoint/Main.java
@@ -1,36 +1,9 @@
 package org.ryoo.knimeEbi.entryPoint;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
-import org.processmining.ebi.CallEbi;
 import org.ryoo.knimeEbi.scaffolder.EbiNodeGenerator;
 
 public class Main {
 	public static void main(String[] args) {
 		EbiNodeGenerator.generateEbiNodes();
-		/*
-		String ebiCommands = CallEbi.call_ebi("Ebi itself java", "text", new String[0]);
-		
-		Path outputPath = Path.of(
-			"src",
-			"org",
-			"ryoo",
-			"knimeEbi",
-			"scaffolder",
-			"ebi-itself-java-output.txt"
-		);
-			
-		try {
-			Files.writeString(outputPath, ebiCommands, StandardCharsets.UTF_8);
-		} catch (IOException e) {
-			System.out.println("Error writing output of \"Ebi itself java\" to txt-file.");
-			e.printStackTrace();
-		}*/
 	}
-	
-	
-
 }
