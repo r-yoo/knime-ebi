6075eaea8bf2d0dbbe5772d05c75525baa9f6d66
Reinhold Yoo <reinhold.yoo@rwth-aachen.de>
2026-09-14 14:53:46 +0200

    Final cleanup

---
+++ org.ryoo.knimeEbi/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.ryoo.knimeEbi.entryPoint","l":"Main"}];updateSearchResults();
--- org.ryoo.knimeEbi/src/org/ryoo/knimeEbi/entryPoint/Main.java
+++ org.ryoo.knimeEbi/src/org/ryoo/knimeEbi/entryPoint/Main.java
@@ -1,32 +1,9 @@
 package org.ryoo.knimeEbi.entryPoint;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
-import org.processmining.ebi.CallEbi;
 import org.ryoo.knimeEbi.scaffolder.EbiNodeGenerator;
 
 public class Main {
 	public static void main(String[] args) {
 		EbiNodeGenerator.generateEbiNodes();
-		
-//		Path eventLogPath = Path.of( 
-//			"src",
-//			"org",
-//			"ryoo",
-//			"knimeEbi",
-//			"scaffolder",
-//			"TicketingManagementLog.xes"
-//		);
-//		
-//		try {
-//			String xesContent = Files.readString(eventLogPath, StandardCharsets.UTF_8);
-//			CallEbi.call_ebi("Ebi discover-non-stochastic flower process-tree", "pnml", new String[] {xesContent});
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
 	}
 }
